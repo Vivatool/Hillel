a3325142e927f05aa56a631d87ecd0424cb2b9cf
Denis Yermolin <34105970+Vivatool@users.noreply.github.com>
2018-01-02 17:41:43 +0200

    Create 1.php

---
+++ HM/homework 3/1.php
@@ -0,0 +1,19 @@
+<?php
+$a = ['abracadabra', '23', 'rrr'];
+
+
+function tudasuda($a)
+{
+
+    $b = [];
+
+    while ($b[] = array_pop($a)) {
+
+    }
+    array_pop($b);
+
+    return $b;
+
+}
+
+var_dump(tudasuda($a));
