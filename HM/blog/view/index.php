20b934eac4fab886a092d297835ad542bb755938
Denis Yermolin <34105970+Vivatool@users.noreply.github.com>
2018-01-17 17:39:36 +0200

    Update index.php

--- HM/blog/view/index.php
+++ HM/blog/view/index.php
@@ -15,7 +15,7 @@
         <div class="col-md-10">
           <a href="/"><h1>Hello, user!</h1></a>
           <a href="/?r=/addPost">add new post</a><br>
-            <a href="/?r=/getTitles">Oll titles</a><br>
+            <a href="/?r=/getTitles">all titles</a><br>
         </div>
         <?php foreach ($posts as $index => $post) { ?>
           <div class="col-md-10">
