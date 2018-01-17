52010a8799a5347256a0c00b158eb981c6343e8e
Denis Yermolin <34105970+Vivatool@users.noreply.github.com>
2018-01-17 17:35:59 +0200

    Update index.php

--- HM/blog/index.php
+++ HM/blog/index.php
@@ -8,8 +8,8 @@
   require_once './actions/add_post_page.php';
   require_once './actions/delete_page.php';
   require_once './actions/get_titles.php';
-  require_once './actions/get_even_posts.php';
-  require_once './actions/get_sorted_posts.php';
+
+
 
   $router = new Router($_GET, $_POST);
 
@@ -18,7 +18,7 @@
   $router->attach('addPostPage', new AddPostPage());
   $router->attach('deletePostPage', new DeletePostPage());
   $router->attach('getTitles', new GetTitles());
-  $router->attach('getEvenPosts', new GetEvenPosts());
-  $router->attach('getSortedPosts', new GetSortedPosts());
+
+
 
   $router->serve();
